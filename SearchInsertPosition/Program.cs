1d9c84edb1e731cc25c02247b07177fc19d0bf03
Mika Mirzanasiri <mika-mm@hotmail.com>
2023-02-16 15:25:04 +0000

    Add project files.

---
+++ SearchInsertPosition/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Solution.SearchInsert(new int[] { 1, 3, 5, 6 }, 7));
+    }
+}
+
+public class Solution
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target || nums[i] > target) return i;
+            else continue;
+        }
+        return nums.Length;
+    }
+}
